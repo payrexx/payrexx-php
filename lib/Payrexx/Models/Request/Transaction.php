c326f41d0a76575c5fefa3fed14d359f45bfc5d9
Michael Räss <raess.michael@gmail.com>
2018-06-21 16:26:22 +0200

    Added amount to transaction model

--- lib/Payrexx/Models/Request/Transaction.php
+++ lib/Payrexx/Models/Request/Transaction.php
@@ -15,6 +15,25 @@ namespace Payrexx\Models\Request;
  */
 class Transaction extends \Payrexx\Models\Base
 {
+    /** @var int $amount */
+    protected $amount;
+
+    /**
+     * @return int
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param int $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
     /**
      * {@inheritdoc}
      */
