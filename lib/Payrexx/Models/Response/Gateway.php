965c6f137d29f84e73767ad2f0b61b1d73a0a371
vinoth <vinoth@payrexx.com>
2025-11-11 17:19:53 +0530

    hotfix/PP-17365: add locale to link

--- lib/Payrexx/Models/Response/Gateway.php
+++ lib/Payrexx/Models/Response/Gateway.php
@@ -52,6 +52,10 @@ class Gateway extends \Payrexx\Models\Request\Gateway
 
     public function setLink(string $link): void
     {
+        $language = $this->getLanguage();
+        if (!empty($language) && strpos($link, "/{$language}/") === false) {
+            $link = str_replace('/?', "/{$language}/?", $link);
+        }
         $this->link = $link;
     }
 
--- lib/Payrexx/Payrexx.php
+++ lib/Payrexx/Payrexx.php
@@ -19,7 +19,7 @@ use Payrexx\Models\Base;
  */
 class Payrexx
 {
-    public const CLIENT_VERSION = '2.0.3';
+    public const CLIENT_VERSION = '2.0.4';
 
     protected Communicator $communicator;
 
