68c21cf6e3ffe8b67188de8c8f10d6bdbbf7fe49
vinoth <vinoth@payrexx.com>
2025-12-19 11:07:13 +0530

    bugfix/PP-17899: updated the plugin version

--- lib/Payrexx/Payrexx.php
+++ lib/Payrexx/Payrexx.php
@@ -19,7 +19,7 @@ use Payrexx\Models\Base;
  */
 class Payrexx
 {
-    public const CLIENT_VERSION = '2.0.5';
+    public const CLIENT_VERSION = '2.0.7';
 
     protected Communicator $communicator;
 
