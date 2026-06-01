5a10ce14f94cc3ea5b377e407ac54bfdaa9091a7
vinothss4u <vinoth@payrexx.com>
2026-06-01 14:12:19 +0530

    bugfix/PP-19333: version change and removed transaction create.

--- lib/Payrexx/Payrexx.php
+++ lib/Payrexx/Payrexx.php
@@ -19,7 +19,7 @@ use Payrexx\Models\Base;
  */
 class Payrexx
 {
-    public const CLIENT_VERSION = '2.0.13';
+    public const CLIENT_VERSION = '2.0.14';
 
     protected Communicator $communicator;
 
